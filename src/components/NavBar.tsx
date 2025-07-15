ae9331dd2a9ff168244d68890a959fe5457fac21
AlonsoGaray <alonsogarayp@gmail.com>
2025-07-14 21:11:40 -0500

    logo

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,8 +5,8 @@ const NavBar = () => {
   const navigate = useNavigate();
   
   return (
-  <nav className={`flex w-full items-center justify-between bg-white shadow px-8 h-14`}>
-    <div className="text-2xl font-bold text-gray-800 tracking-wide">HappyMerch</div>
+  <nav className="flex w-full items-center justify-between bg-white shadow px-8 h-14">
+    <img className="h-fit max-h-10" src="/Logo.svg" alt="logo" />
     <div className="flex items-center space-x-4">
       <button className="bg-gray-100 rounded-md px-4 py-2 text-base hover:bg-gray-200 transition">Nuevo</button>
       <button className="bg-gray-100 rounded-md px-4 py-2 text-base hover:bg-gray-200 transition">Guardar</button>
